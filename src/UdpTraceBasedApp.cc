7ecf16b4d341d64e6a200dfddf8538fcb425d60a
Musab Ahmed <musab.ahmed@tuhh.de>
2022-06-13 10:30:28 +0200

    clean repo from changes to add hop count

--- src/UdpTraceBasedApp.cc
+++ src/UdpTraceBasedApp.cc
@@ -2,14 +2,6 @@
 
 #include "inet/common/ModuleAccess.h"
 #include "inet/common/lifecycle/NodeStatus.h"
-//////////////////////////////////////////////////////////////////////////
-// Hop Count Signal (Musab)
-//////////////////////////////////////////////////////////////////////////
-#include "inet/networklayer/common/FragmentationTag_m.h"
-#include "inet/applications/base/ApplicationPacket_m.h"
-#include "inet/common/TimeTag_m.h"
-
-
 
 Define_Module(UdpTraceBasedApp);
 
@@ -26,10 +18,6 @@ void UdpTraceBasedApp::initialize(int stage)
         // Emit application packet sent Signal (Musab)
         //////////////////////////////////////////////////////////////////////////
         appPacketSentSignal = registerSignal("appPacketSent");
-        //////////////////////////////////////////////////////////////////////////
-        // Register Hop Count Signal (Musab)
-        //////////////////////////////////////////////////////////////////////////
-        hopCountSignal = registerSignal("hopCount");
     }
 }
 
@@ -75,31 +63,6 @@ void UdpTraceBasedApp::processStart(){
     UdpBasicApp::processStart();
 }
 
-void UdpTraceBasedApp::sendPacket()
-{
-    std::ostringstream str;
-    str << packetName << "-" << numSent;
-    Packet *packet = new Packet(str.str().c_str());
-    if(dontFragment)
-        packet->addTag<FragmentationReq>()->setDontFragment(true);
-    const auto& payload = makeShared<ApplicationPacket>();
-    payload->setChunkLength(B(par("messageLength")));
-    payload->setSequenceNumber(numSent);
-    payload->addTag<CreationTimeTag>()->setCreationTime(simTime());
-    packet->insertAtBack(payload);
-    //////////////////////////////////////////////////////////////////////////
-    // Emit Hop Count Signal (Musab)
-    //////////////////////////////////////////////////////////////////////////
-    // Initially create a packet with a hop count value of 0 (Musab)
-    auto HopCountTag = packet->addTag<hopCountTag>();
-    HopCountTag->setHopCount(0);
-    EV_INFO << "Current hop count = " << HopCountTag->getHopCount() << endl;
-    L3Address destAddr = chooseDestAddr();
-    emit(packetSentSignal, packet);
-    socket.sendTo(packet, destAddr, destPort);
-    numSent++;
-}
-
 void UdpTraceBasedApp::processSend()
 {
     sendPacket();
@@ -118,49 +81,4 @@ void UdpTraceBasedApp::processSend()
         selfMsg->setKind(STOP);
         scheduleAt(simTime(), selfMsg);
     }
-}
-
-void UdpTraceBasedApp::processPacket(Packet *pk)
-{
-    emit(packetReceivedSignal, pk);
-    EV_INFO << "Received packet: " << UdpSocket::getReceivedPacketInfo(pk) << endl;
-    //////////////////////////////////////////////////////////////////////////
-    // Emit Hop Count Signal (Musab)
-    //////////////////////////////////////////////////////////////////////////
-    auto HopCountTag = pk->getTag<hopCountTag>();
-    EV_INFO << "Hop count for application packet = " << endl;
-    EV_INFO << "Hop count for application packet = " << HopCountTag->getHopCount() << endl;
-    delete pk;
-    numReceived++;
-}
-
-void UdpTraceBasedApp::socketDataArrived(UdpSocket *socket, Packet *packet)
-{
-    // process incoming packet
-    processPacket(packet);
-}
-
-void UdpTraceBasedApp::handleMessageWhenUp(cMessage *msg)
-{
-    if (msg->isSelfMessage()) {
-        ASSERT(msg == selfMsg);
-        switch (selfMsg->getKind()) {
-            case START:
-                processStart();
-                break;
-
-            case SEND:
-                processSend();
-                break;
-
-            case STOP:
-                processStop();
-                break;
-
-            default:
-                throw cRuntimeError("Invalid kind %d in self message", (int)selfMsg->getKind());
-        }
-    }
-    else
-        socket.processMessage(msg);
 }
