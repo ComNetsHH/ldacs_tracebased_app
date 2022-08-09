20ebb25c78ff8e2189a9fe32eb441e06a191ba61
Musab Ahmed <musab.ahmed@tuhh.de>
2022-08-09 13:05:39 +0200

    add multi ground stations first commit

--- src/UdpTraceBasedApp.cc
+++ src/UdpTraceBasedApp.cc
@@ -2,6 +2,8 @@
 
 #include "inet/common/ModuleAccess.h"
 #include "inet/common/lifecycle/NodeStatus.h"
+#include "inet/common/geometry/common/Coord.h"
+#include "inet/common/lifecycle/ModuleOperations.h"
 
 Define_Module(UdpTraceBasedApp);
 
@@ -14,6 +16,26 @@ void UdpTraceBasedApp::initialize(int stage)
         // read the timeStamp vector and in case it it empty or the file does not exist the module will not be started,
         // as specified in handleStartOperation method.
         parseTraceFile2Vector(traceFile,timeStamp);
+        //////////////////////////////////////////////////////////////////////////
+        // Allow multuple groundstations (Musab) 
+        //////////////////////////////////////////////////////////////////////////
+        multiGroundStationUsed = par("multiGroundStationUsed").boolValue();
+        GSx = par("GSx");
+        GSy = par("GSy");
+        GSz = par("GSz");
+        GS2x = par("GS2x");
+        GS2y = par("GS2y");
+        GS2z = par("GS2z");
+        std::vector<std::vector<double>> locations
+        {
+            { GSx, GSy, GSz },
+            { GS2x, GS2y, GS2z }
+        };
+        GSLocations = locations;
+        if(multiGroundStationUsed){
+            host = getContainingNode(this);
+            mobility = check_and_cast<IMobility *>(host->getSubmodule("mobility"));
+        }
         //////////////////////////////////////////////////////////////////////////
         // Emit application packet sent Signal (Musab)
         //////////////////////////////////////////////////////////////////////////
@@ -88,4 +110,50 @@ void UdpTraceBasedApp::processSend()
         selfMsg->setKind(STOP);
         scheduleAt(simTime(), selfMsg);
     }
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Allow multuple groundstations (Musab) 
+//////////////////////////////////////////////////////////////////////////
+L3Address UdpTraceBasedApp::chooseDestAddr()
+{
+    int k = intrand(destAddresses.size());
+    if(multiGroundStationUsed) {
+        const Coord GroundStationLocation = Coord(GSx, GSy, GSz);
+        const Coord GroundStationLocation2 = Coord(GS2x, GS2y, GS2z);
+        m distanceToGroundStation = m(mobility->getCurrentPosition().distance(GroundStationLocation));
+        m distanceToGroundStation2 = m(mobility->getCurrentPosition().distance(GroundStationLocation2));
+        if (distanceToGroundStation < distanceToGroundStation2)
+            return destAddresses[0];
+        else
+            return destAddresses[1];
+        // for (int i = 0; i < GSLocations.size(); i++)
+        // {
+        //     EV_INFO << "Ground station (Destination) position x = " << GSLocations[i][0] << " y = " << GSLocations[i][1] << " z = " << GSLocations[i][2] << endl;
+        // }
+        // return;
+    }
+    if (destAddresses[k].isUnspecified() || destAddresses[k].isLinkLocal()) {
+        L3AddressResolver().tryResolve(destAddressStr[k].c_str(), destAddresses[k]);
+    }
+    return destAddresses[k];
+}
+
+void UdpTraceBasedApp::sendPacket()
+{
+    std::ostringstream str;
+    str << packetName << "-" << numSent;
+    Packet *packet = new Packet(str.str().c_str());
+    if(dontFragment)
+        packet->addTag<FragmentationReq>()->setDontFragment(true);
+    const auto& payload = makeShared<ApplicationPacket>();
+    payload->setChunkLength(B(par("messageLength")));
+    payload->setSequenceNumber(numSent);
+    payload->addTag<CreationTimeTag>()->setCreationTime(simTime());
+    packet->insertAtBack(payload);
+    L3Address destAddr = chooseDestAddr();
+    emit(packetSentSignal, packet);
+    socket.sendTo(packet, destAddr, destPort);
+    numSent++;
 }
--- src/UdpTraceBasedApp.h
+++ src/UdpTraceBasedApp.h
@@ -28,6 +28,16 @@
 #include "inet/common/ModuleAccess.h"
 #include "inet/common/lifecycle/NodeStatus.h"
 #include "inet/common/lifecycle/ModuleOperations.h"
+#include "inet/networklayer/common/L3AddressResolver.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/applications/base/ApplicationPacket_m.h"
+#include "inet/common/TagBase_m.h"
+#include "inet/common/TimeTag_m.h"
+#include "inet/networklayer/common/FragmentationTag_m.h"
+#include "inet/transportlayer/contract/udp/UdpControlInfo_m.h"
+#include "inet/mobility/contract/IMobility.h"
+
+
 
 
 using namespace inet;
@@ -43,6 +53,21 @@ protected:
     void handleStartOperation(LifecycleOperation *operation);
     int timeIndex;
     std::vector<double> timeStamp;
+    //////////////////////////////////////////////////////////////////////////
+    // Allow multuple groundstations (Musab) 
+    //////////////////////////////////////////////////////////////////////////
+    bool multiGroundStationUsed = false;
+    cModule *host = nullptr;
+    IMobility *mobility = nullptr;
+    virtual void sendPacket() override;
+    virtual L3Address chooseDestAddr() override; // chooses random destination address and is multiGS choose closest in distance
+    std::vector<std::vector<double> > GSLocations;
+    double GSx = 0.0;
+    double GSy = 0.0;
+    double GSz = 0.0;
+    double GS2x = 0.0;
+    double GS2y = 0.0;
+    double GS2z = 0.0;
     //////////////////////////////////////////////////////////////////////////
     // Emit application packet sent Signal (Musab)
     //////////////////////////////////////////////////////////////////////////
