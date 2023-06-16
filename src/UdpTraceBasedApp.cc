2ce4913366568e7191a0d99629546f2b49158eaf
Sebastian Lindner <sebastian.lindner@tuhh.de>
2023-06-16 15:29:45 +0200

    add license, readme, copyright notices

--- src/UdpTraceBasedApp.cc
+++ src/UdpTraceBasedApp.cc
@@ -1,3 +1,19 @@
+// The L-Band Digital Aeronautical Communications System (LDACS) Trace-Based App implements a trace-based application in OMNeT++ for the LDACS Air-Air Medium Access Control simulator.
+// Copyright (C) 2023  Sebastian Lindner, Konrad Fuger, Musab Ahmed Eltayeb Ahmed, Andreas Timm-Giel, Institute of Communication Networks, Hamburg University of Technology, Hamburg, Germany
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include "UdpTraceBasedApp.h"
 
 #include "inet/common/ModuleAccess.h"
--- src/UdpTraceBasedApp.h
+++ src/UdpTraceBasedApp.h
@@ -1,10 +1,10 @@
+// The L-Band Digital Aeronautical Communications System (LDACS) Trace-Based App implements a trace-based application in OMNeT++ for the LDACS Air-Air Medium Access Control simulator.
+// Copyright (C) 2023  Sebastian Lindner, Konrad Fuger, Musab Ahmed Eltayeb Ahmed, Andreas Timm-Giel, Institute of Communication Networks, Hamburg University of Technology, Hamburg, Germany
 //
-// Copyright (C) 2000 Institut fuer Telematik, Universitaet Karlsruhe
-// Copyright (C) 2004,2011 Andras Varga
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,8 +12,7 @@
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with this program; if not, see <http://www.gnu.org/licenses/>.
-//
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef __INET_UDPTRACEBASEDAPP_H
 #define __INET_UDPTRACEBASEDAPP_H
